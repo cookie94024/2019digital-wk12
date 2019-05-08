21607ba8eba65f5b0dc762e7e2d117a989e5ed34
cookie94024 <48972540+cookie94024@users.noreply.github.com>
2019-05-08 11:47:49 +0800

    Add files via upload

---
+++ 第9組110719028李逸歆110719029劉宇蓁.ino
@@ -0,0 +1,25 @@
+int stateBt=0;
+int btPin=2;
+int btLed=13;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(btPin,INPUT);
+pinMode(btLed,OUTPUT);
+digitalWrite(btPin,HIGH);
+}
+
+void loop() {
+  stateBt=digitalRead(btPin);
+  if(stateBt==HIGH){
+    digitalWrite(btLed,LOW);
+    }
+    else{
+      digitalWrite(btLed,HIGH);
+      }
+  // put your main code here, to run repeatedly:
+
+}
+
+
+
